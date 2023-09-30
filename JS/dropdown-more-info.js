13128241430d8e4105030744c4ee4d2143e8cbb9
Ian Smith <iannsmif@gmail.com>
2023-09-30 17:58:42 -0500

    MODIFY-PAGE: firebase login auth

--- JS/dropdown-more-info.js
+++ JS/dropdown-more-info.js
@@ -5,9 +5,16 @@ buttons.forEach((button) => {
     button.addEventListener("click", toggleInfo);
 } )
 
+// Step 1: Check if the button has the class ".btn--important"
+
+
 function toggleInfo(event) {
     const button = event.currentTarget;
     const infoBox = button.nextElementSibling;
+    if (button.classList.contains("btn--important")) {
+        // Step 2: Select the next sibling element and change its border color to red
+        button.nextElementSibling.style.borderColor = "coral";
+    }
 
     if (!isInfoBoxOpen) {
         infoBox.style.animationName = "bounceDown";
